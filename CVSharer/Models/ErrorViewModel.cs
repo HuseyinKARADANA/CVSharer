9dfdebfff17a35846d647a129f8f0b0f465c9ed9
Hüseyin Karadana <huseyinkaradana35@gmail.com>
2023-08-27 23:56:48 +0300

    project start

---
+++ CVSharer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CVSharer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
