d30a827c9526905ba39e246de6e2584add6b410d
emirmorali <morali.emir@hotmail.com>
2023-08-28 18:48:23 +0300

    -Register işlemi başarılı -Küçük frontend düzenlemeleri

--- CVSharer/Controllers/SessionController.cs
+++ CVSharer/Controllers/SessionController.cs
@@ -1,17 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using EntityLayer.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
 
 namespace CVSharer.Controllers
 {
     public class SessionController : Controller
     {
+        private readonly IUserService _userService;
+        private readonly IToastNotification _toast;
+
+
+        public SessionController(IUserService userService, IToastNotification toast)
+        {
+            _userService = userService;
+            _toast = toast;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
         [HttpPost]
-        public IActionResult Register(string s)
+        public IActionResult Register(RegisterDTO registerDTO)
         {
+            List<User> users = _userService.GetListAll();
+
+            if(users.Any(u => u.Email == registerDTO.Email))
+            {
+                _toast.AddErrorToastMessage("There is a user with this email. Please try another email.", new ToastrOptions { Title = "Error" });
+                return View();
+            }
+
+            if(!registerDTO.Password.Equals(registerDTO.PasswordAgain))
+            {
+                _toast.AddErrorToastMessage("Passwords do not match!", new ToastrOptions { Title = "Error" });
+                return View();
+            }
+
+            if(registerDTO.Password.Length < 8)
+            {
+                _toast.AddErrorToastMessage("Your password can be minimum 8 characters!", new ToastrOptions { Title = "Error" });
+                return View();
+            }
+
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(registerDTO.Password);
+
+            _userService.Insert(new User()
+            {
+                Name = registerDTO.Name,
+                Surname = registerDTO.Surname,
+                Email = registerDTO.Email,
+                Password = hashedPassword,
+            });
+
+            _toast.AddSuccessToastMessage("Register Successful.", new ToastrOptions { Title = "Successful" });
+
             return RedirectToAction("Login");
         }
 
@@ -21,7 +68,7 @@ namespace CVSharer.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Login(string s)
+        public IActionResult Login(LoginDTO loginDTO)
         {
             return RedirectToAction("Login");
         }
--- CVSharer/Program.cs
+++ CVSharer/Program.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Abstract;
 using DataAccessLayer.Concrete.EntityFramework;
 using DataAccessLayer.Contexts;
 using Microsoft.EntityFrameworkCore;
+using NToastNotify;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
 });
+
+//Add Toast Message Services to the container.
+builder.Services.AddControllersWithViews()
+    .AddNToastNotifyToastr(new ToastrOptions()
+    {
+        PositionClass = ToastPositions.TopRight,
+        TimeOut = 8000,
+        ProgressBar = true,
+    });
 
 //Certificate Dependency Injection
 builder.Services.AddScoped<ICertificateService, CertificateManager>();
@@ -56,6 +66,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseNToastNotify(); //toast 
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
