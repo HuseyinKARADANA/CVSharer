829c5fbc293a6fcc9166b04db3077a601fa7a5bf
Hüseyin Karadana <huseyinkaradana35@gmail.com>
2023-09-01 17:18:25 +0300

    version3.6

---
+++ CVSharer/Controllers/PdfController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVSharer.Controllers
+{
+    public class PdfController : Controller
+    {
+		private readonly IUserService _userService;
+
+		public PdfController(IUserService userService)
+		{
+			_userService = userService;
+		}
+
+		public IActionResult BaseTemplate(string sharecode)
+        {
+			var user = _userService.GetUserByShareCode(sharecode);
+			return View(user);
+		}
+    }
+}
