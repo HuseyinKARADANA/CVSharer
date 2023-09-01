e80717caeac50398fa8a02802178171309ed6d1f
Hüseyin Karadana <huseyinkaradana35@gmail.com>
2023-09-01 17:20:26 +0300

    version 3.6.1

--- CVSharer/Controllers/DashboardController.cs
+++ CVSharer/Controllers/DashboardController.cs
@@ -484,7 +484,7 @@ namespace CVSharer.Controllers
 			var links = _linkService.GetLinksByUserId(int.Parse(userId));
 
 			var fileName = user.Name + " " + user.Surname + " CV.pdf";
-			string url = "https://www.cvsharer.com//Pdf/BaseTemplate?ShareCode=" + user.ShareCode;
+			string url = "https://www.cvsharer.com/Pdf/BaseTemplate?ShareCode=" + user.ShareCode;
             string htmlContent="";
 			// HTTP isteği gönderin ve yanıtı alın
 			using (HttpClient client = new HttpClient())
